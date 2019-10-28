9ca0160e3bf213d7df9febe5b90b3ce827a0855d
Lior Shefer <liorshe@wix.com>
2019-10-28 13:38:34 -0400

    get default images from a file

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
 
 export default {
     input: ['index.js'],
@@ -10,6 +11,7 @@ export default {
     },
     plugins: [
         resolve(),
+        json(),
         babel()
     ]
 };
